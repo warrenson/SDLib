a787c9d09324f335842a44abee4c7c3f5f0157fc
Ben Warren <ben.warren@plantandfood.co.nz>
2018-10-18 16:11:02 +1300

    tabulate 1M results

---
+++ results/tabulate2.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+#
+# 0.2-0.05_kmeans_reverse-bandwagon_p1.res.txt
+use strict;
+
+my $file = $ARGV[0];
+
+my $name = `basename $file .res.txt`;
+chomp $name;
+
+my ($method, $attack, $param) = split('_', $name); 
+$param ||= 'NA';
+#my ($attack_size, $filler_size) = split('-', $sizes);
+
+my $lastline = `tail -n1 $file`;
+$lastline =~ s/^\s+|\s+$//g;
+my ($label, $pres, $rec, $f1, $sup) = split(/\s+/, $lastline);
+
+# HEADER
+# METHOD,PARAMS,ATTACK,ATTACK-SIZE,FILLER-SIZE,PRECISION,RECALL,F1,SUPPORT
+print join(',',
+    $method,
+    $param,
+    $attack,
+    $pres,
+    $rec,
+    $f1,
+    $sup
+), "\n";
